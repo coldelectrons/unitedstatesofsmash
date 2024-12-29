1dfa0fd47b9d47d9eb11a09259f32a39711c223f
coldelectrons <frithomas@gmail.com>
2024-12-28 17:33:16 -0800

    added fatso

---
+++ modules/nixos/hardware/nofio-wireless/default.nix
@@ -0,0 +1,30 @@
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.hardware.nofio-wireless;
+  user = config.${namespace}.user;
+  home = config.users.users.${user.name}.home;
+in
+{
+  options.${namespace}.hardware.nofio-wireless = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Nofio Wireless support.";
+  };
+
+  config = mkIf cfg.enable {
+
+    boot.kernelModules = [ "vhci-hcd" ];
+    boot.extraModulePackages = with config.boot.kernelPackages; [ usbip ];
+    
+
+    environment.systemPackages = with pkgs; [
+    ];
+  };
+}
---
+++ systems/x86_64-linux/fatso/default.nix
@@ -0,0 +1,92 @@
+{ pkgs
+, config
+, lib
+, channel
+, namespace
+, inputs
+, ...
+}:
+with lib;
+with lib.${namespace};
+{
+  imports = [ ./hardware.nix ];
+
+  # Resolve an issue with hades's wired connections failing sometimes due to weird
+  # DHCP issues. I'm not quite sure why this is the case, but I have found that the
+  # problem can be resolved by stopping dhcpcd, restarting Network Manager, and then
+  # unplugging and replugging the ethernet cable. Perhaps there's some weird race
+  # condition when the system is coming up that causes this.
+  networking.dhcpcd.enable = false;
+
+
+  boot = {
+    binfmt.emulatedSystems = [  ];
+    consoleLogLevel = 0;
+    kernelParams = [ 
+    ];                                                                                                                                   
+    # plymouth.enable = true;
+    # Swapfile hibernate
+    # resumeDevice = "${MAIN_PART}";
+    # kernelParams = [ "resume_offset=${RESUME_OFFSET}" "nvidia_drm.fbdev=1" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  nix.settings.trusted-users = [ "root" "coldelectrons" "nix-ssh" ];
+
+  plusultra = {
+    nix = enabled;
+    archetypes = {
+      server = enabled;
+    };
+    cli-apps = {
+      extras = enabled;
+    };
+    security = {
+      acme = enabled;
+      gpg = enabled;
+    };
+    apps = {
+    };
+    hardware = {
+    };
+    services = {
+    };
+    system = {
+    };
+
+    user.extraGroups = [ 
+      "networkmanager"
+      "wheel"
+      "input"
+      "plugdev"
+      "dialout"
+      "video"
+      "audio"
+      "libvirtd"
+      "scanner"
+      "i2c"
+      "git"
+    ];
+  };
+
+
+  # WiFi is typically unused on the desktop. Enable this service
+  # if it's no longer only using a wired connection.
+  # systemd.services.network-addresses-wlp41s0.enable = false;
+
+  # something is making me wait 120 seconds, and I *hate* it
+  systemd.network.wait-online.timeout = 0;
+  systemd.network.wait-online.enable = false;
+  boot.initrd.systemd.network.wait-online.timeout = 0;
+  boot.initrd.systemd.network.wait-online.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
