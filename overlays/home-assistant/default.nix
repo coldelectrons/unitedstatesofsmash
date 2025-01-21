9954fee5c920a81ff9a99c24141fcd846dfc1e5c
coldelectrons <frithomas@gmail.com>
2025-01-21 08:33:51 -0800

    added HA overlay with attempt at import module

---
+++ overlays/home-assistant/default.nix
@@ -0,0 +1,12 @@
+{ channels, ... }:
+
+final: prev:
+
+# 20250103 due to a bug with curl-cffi, `--impersonate` is not available in 24.11
+# the patch was merged into nixpkgs:master, but not yet in unstable
+{ inherit (channels.unstable) home-assistant;
+  disabledModules = [
+    "services/home-automation/home-assistant.nix"
+  ];
+  imports = [ channels.unstable/nixos/modules/services/home-automation/home-assistant.nix ];
+}
